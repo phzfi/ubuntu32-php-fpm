89fce7daa79a192c6393c752ea4eeeaffb3a0ac8
Antti Hauhtonen <hauhto@phz.fi>
2021-08-13 11:26:25 +0300

    Started versioning for different PHP-versions

--- docker/build.sh
+++ docker/build.sh
@@ -2,28 +2,33 @@
 VERSION=$1
 USER=$2
 PASSWORD=$3
+BRANCH=$4
 
 if [ -z "$VERSION" ]; then
-  echo "Usage: ./build.sh <version>, e.g. ./build.sh latest user password"
+  echo "Usage: ./build.sh <version>, e.g. ./build.sh latest user password php-version"
   exit 1
 fi
 if [ -z "$USER" ]; then
-  echo "Usage: ./build.sh <version>, e.g. ./build.sh latest user password"
+  echo "Usage: ./build.sh <version>, e.g. ./build.sh latest user password php-version"
   exit 1
 fi
 if [ -z "$PASSWORD" ]; then
-  echo "Usage: ./build.sh <version>, e.g. ./build.sh latest user password"
+  echo "Usage: ./build.sh <version>, e.g. ./build.sh latest user password php-version"
+  exit 1
+fi
+if [ -z "$BRANCH" ]; then
+  echo "Usage: ./build.sh <version>, e.g. ./build.sh latest user password php-version"
   exit 1
 fi
 
 #login to docker hub
 docker login -u $USER -p $PASSWORD
 
-IMAGE=phzfi/ubuntu32-php-fpm:$VERSION
-LATEST=phzfi/ubuntu32-php-fpm:latest
+IMAGE=phzfi/ubuntu32-php-fpm:$BRANCH-$VERSION
+LATEST=phzfi/ubuntu32-php-fpm:$BRANCH-latest
 echo "Building $IMAGE"
 
-docker build -t $IMAGE -f Dockerfile . --no-cache
+docker build -t $IMAGE -f Dockerfile-$BRANCH . --no-cache
 docker tag $IMAGE $IMAGE
 docker tag $IMAGE $LATEST
 docker push $IMAGE
